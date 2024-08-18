4a1610b0ac2085bb8c97d92b5c496e5215f3b3d9
casiocompa <casiocompa@gmail.com>
2024-08-18 20:35:42 +0200

    Initial Commit

---
+++ RozetkaPay.Example.SwiftUI/RozetkaPay_Example_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  RozetkaPay_Example_SwiftUIApp.swift
+//  RozetkaPay.Example.SwiftUI
+//
+//  Created by Ruslan Kasian Dev on 18.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct RozetkaPay_Example_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
